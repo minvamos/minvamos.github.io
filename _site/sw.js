52bcb72f8ce7aa90510b30d911995250227a6fe8
zzmsz <zzmsz0317@gmail.com>
2023-01-07 01:14:51 +0900

    sd

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230107.000835';
+const cacheName = 'chirpy-20230107.001211';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
